1b0958ad6c98e810c0759d792d03f934128208c5
mozhgan-k <mozhan.kabki@gmail.com>
2025-02-02 21:06:43 +0330

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -9,5 +9,22 @@ export interface ITicker {
   ts: number,
   vol_24h: number;
   vol_24h_pair: number;
-  difference_24: number
+  difference_24: number;
+  formattedPrice: string
+}
+
+export interface ILiveData {
+  best_ask: string;
+  best_bid: number;
+  high24h: number
+  last: string;
+  low24h: number;
+  open_24h: string;
+  symbol: string;
+  instId: string;
+  ts: number,
+  vol_24h: number;
+  vol_24h_pair: number;
+  difference_24: number;
+  formattedPrice: string
 }
