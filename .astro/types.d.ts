5de797887c2362ce7b4cb0e62206f48c027d2f34
Mozhgan <mozhan.kabki@gmail.com>
2025-02-03 14:58:16 +0330

    Update: faq.astro

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -1,9 +1,1 @@
 /// <reference types="astro/client" />
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-/// <reference path="content.d.ts" />
->>>>>>> Stashed changes
-=======
-/// <reference path="content.d.ts" />
->>>>>>> Stashed changes
